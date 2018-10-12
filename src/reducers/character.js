0297c155f29a2512b9815f16237f585e3e4556f8
YuriSS <yuri.s.dos.santos@gmail.com>
2018-10-12 00:51:24 -0300

    adicionado abstracao de reducers de dados unicos

--- src/reducers/character.js
+++ src/reducers/character.js
@@ -4,37 +4,13 @@ import {
     NEXT_CHAR,
     PREV_CHAR
 } from '../actions/character'
+import createData from './data'
 
-import { Some, None, fromNull } from '../helpers/Option'
-
-import { combineReducers } from 'redux'
-
-import stack from './stack'
-
-const current = (
-    state={
-        data: None,
-        error: None
-    },
-    action
-) => {
-    switch (action.type) {
-        case (CHAR_SELECTED):
-            return Object.assign({}, state, { data: fromNull(action.char), error: None })
-        case (NEXT_CHAR):
-            return Object.assign({}, state, { data: None, error: None })
-        case (PREV_CHAR):
-            return Object.assign({}, state, { data: None, error: None })
-        case (CHAR_FAILED):
-            return Object.assign({}, state, { error: Some('Ferrou') })
-        default:
-            return state
-    }
-}
-
-const character = combineReducers({
-    current,
-    number: stack({ NEXT: NEXT_CHAR, PREV: PREV_CHAR })
+export default createData({
+  SELECTED: CHAR_SELECTED,
+  NEXT: NEXT_CHAR,
+  PREV: PREV_CHAR,
+  FAILED: CHAR_FAILED
+}, {
+  key: 'char'
 })
-
-export default character
---
+++ src/reducers/data.js
@@ -0,0 +1,33 @@
+import { Some, None, fromNull } from '../helpers/Option'
+import { combineReducers } from 'redux'
+import stack from './stack'
+
+const createData = (mapActions, mapKeys) => {
+  const current = (
+    state={
+            data: None,
+            error: None
+        },
+        action
+    ) => {
+        switch (action.type) {
+            case (mapActions.SELECTED):
+                return Object.assign({}, state, { data: fromNull(action[mapKeys.key]), error: None })
+            case (mapActions.NEXT):
+                return Object.assign({}, state, { data: None, error: None })
+            case (mapActions.PREV):
+                return Object.assign({}, state, { data: None, error: None })
+            case (mapActions.FAILED):
+                return Object.assign({}, state, { error: Some('Ferrou') })
+            default:
+                return state
+        }
+    }
+
+    return combineReducers({
+        current,
+        number: stack({ NEXT: mapActions.NEXT, PREV: mapActions.PREV })
+    })
+}
+
+export default createData
